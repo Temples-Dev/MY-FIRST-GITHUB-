de9580815e0425273a46b34b8bf31ffaedf6b06a
unknown <templelomotey@gmail.com>
2022-09-08 10:50:49 +0000

    Added Python files

---
+++ Python Files/list.py
@@ -0,0 +1,6 @@
+#list- immutable
+# Define a list of name
+names=["Harry","Ron","Hermione","Ginny"]
+names.append("Draco")
+names.sort()
+print(names)
---
+++ Python Files/sequeneces.py
@@ -0,0 +1,14 @@
+# name="Temple"
+# print(name[5])
+
+names=["Harry","Ron","Hermione"]
+print(names[0])
+
+# cordinateX=10.0
+# cordinateY=20.0
+# cordinate(10.0,20.0)
+
+
+#tuple-
+#set-
+#dict-
